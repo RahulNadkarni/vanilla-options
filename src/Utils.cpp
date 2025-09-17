3f55e7a06d5e68b8010e4da94307a49a290aed07
RahulNadkarni <rnadkarni3@gmail.com>
2025-09-17 17:26:55 -0400

    utils file

---
+++ src/Utils.cpp
@@ -0,0 +1,10 @@
+#include "./include/Utils.h"
+#include <cmath>
+
+double norm_pdf(double x){
+    return 1.0 / std::sqrt(2 * M_PI) * std::exp(-0.5 * x * x); 
+}
+
+double norm_cdf(double x) {
+    return 0.5 * std::erfc(-x / std::sqrt(2)); 
+}
